38e805e28ee35a2986a6a8249131241eba1b5e23
imamtz <imamtz@gmail.com>
2024-12-08 12:43:19 +0700

    Changes from feature2

---
+++ math_util.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    """Adds two numbers and returns the result. 123 """
+    return x + y
+
+def subtract(x, y):
+    """Subtracts two numbers and returns the result."""
+    return x - y
+
+def multiply(x, y): 
+    """Multiplies two numbers and returns the result."""
+    return x * y
+#just a dummy change
+#Added one comment - changed
+#New Commit 
